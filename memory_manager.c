ea3af56a4eb2379d65b867b606761acb738c2d19
Nehuen Lian <nehuenn.lian@gmail.com>
2025-09-24 14:42:38 -0300

    separate memory startup. delete unnecessary comments, add debugging, increment int memory size. fix security and memory bugs

--- memory_manager.c
+++ memory_manager.c
@@ -2,11 +2,44 @@
 #include <stdlib.h>
 #include <string.h>
 
+void startup_memory() {
+
+    FILE *memory = fopen("memory.bin", "wb");
+
+    enum size {SIZE=16}; // 16 bytes
+
+    // struct that will represent all memory structure
+    typedef struct Memory {
+        int *stored_ints;
+        char *stored_chars;
+    } Mem;
+
+    // define 2 memory type structures
+    Mem mem;
+    mem.stored_ints = malloc(SIZE * 4);
+    mem.stored_chars = malloc(SIZE);
+
+    memset(mem.stored_ints, 0, SIZE * 4); 
+    memset(mem.stored_chars, 0, 16);
+
+    // store bytes in the .bin file
+    fwrite(mem.stored_ints, sizeof(int), 16, memory);
+    fwrite(mem.stored_chars, sizeof(char), 16, memory);
+
+    fclose(memory);
+
+    free(mem.stored_ints);
+    free(mem.stored_chars);
+    mem.stored_ints = NULL;
+    mem.stored_chars = NULL;
+}
+
+
 char define_task() {
     char choosed_task;    
     int flag = 1;
 
-    while (flag == 1) { // sentinel loop
+    while (flag == 1) {
         printf("flag: %d\n", flag);
         printf("Define the task type you want to do --\nw: Write\nr: Read.\n");
         scanf("%1s", &choosed_task);
@@ -17,17 +50,18 @@ char define_task() {
         }
         else {
             printf("The task choosed doesn't exist.\n");
-            flag = 1; // flag set to 1 again so the loop stills running
+            flag = 1; // flag set to 1 again so the sentinel loop stills running
         }
     }
     return choosed_task;
 }
 
+
 char define_datatype() {
     char choosed_type = 0;
     int flag = 1;
 
-    while (flag == 1) { // sentinel loop
+    while (flag == 1) {
         printf("flag: %d\n", flag);
         printf("Define datatype you want to access --\ni: int\nc: char\n");
         scanf("%1s",  &choosed_type);
@@ -38,54 +72,51 @@ char define_datatype() {
         }
         else {
             printf("The type choosed doesn't exist.\n");
-            flag = 1; // flag set to 1 again so the loop stills running
+            flag = 1; // flag set to 1 again so the sentinel loop stills running
         }
     }
     return choosed_type;
 }
 
+
+
+int read_binary() { // READ FOR DEBBUGING
+    FILE *binary = fopen("memory.bin", "rb");
+
+    if (!binary) {
+        fclose(binary);
+        perror("Cannot open the file");
+        return 1;
+    }
+
+    unsigned char byte = 0;
+    int bytes_count = 0;
+    while(fread(&byte, 1, 1, binary) == 1) {
+        printf("%d\n", byte);
+        bytes_count++;
+    }
+    printf("total bytes count: %d", bytes_count);
+
+    fclose(binary);
+    return 0;
+}
+
+
 int main() {
 
-// == Program startup ==
-    enum size {SIZE=16}; // 16 bytes
+    FILE *memory = fopen("memory.bin", "r");
 
-    // struct that will represent all memory structure
-    typedef struct Memory {
-        int *store_ints;
-        char *store_chars;
-    } Mem;
-
-    // define 2 memory type structures (16 bytes each)
-    int *arr_int = malloc(SIZE);
-    char *arr_char = malloc(SIZE);
-
-    // initialize both arrays with 0 in all indexes
-    for (int i = 0; i < 4; i++) { // runs 16 / sizeof(int) times
-        arr_int[i] = 0;
+    if (!memory) {
+        startup_memory();
     }
-    for (int i = 0; i < 16; i++) { // runs 16 times: bytes quantity for char
-        arr_char[i] = 0;
+    else {
+        char task = define_task();
+        char type = define_datatype();
+
+        // debugging
+        printf("task in main: %c\n", task);
+        printf("type in main: %c\n", type);
+        read_binary();
     }
-    // memset(arr_int, 0, 16);
-    // memset(arr_char, 0, 16);
-    Mem mem = {arr_int, arr_char}; // store the memories first byte in the struct
-// =========================
-
-    char task = define_task();
-    char type = define_datatype();
-
-    // debugging
-    printf("task in main: %c\n", task);
-    printf("type in main: %c\n", type);
-
-    // verify if the arrays was filled with 0 in all bytes
-    for (int i = 0; i < 16; i++) {
-        printf("i %d: %d\n", i, mem.store_chars[i]);
-    }
-    printf("=======\n");
-    for (int i = 0; i < 4; i++) {
-        printf("i %d: %d\n", i, mem.store_ints[i]);
-    }
-
     return 0;
 }
